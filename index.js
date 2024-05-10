b68bb79f609a6d5c7952526baacfa8ecda00c66c
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-10 17:15:45 +0600

    server updated

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-  res.send('Welcome to Programming Hero Cart Doctor Server');
+  res.send('Car-Doctors Server');
 });
 app.listen(port, () => {
   console.log(`server listening at ${port}`);
